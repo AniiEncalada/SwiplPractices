a084349ec85bd50fcb727fceec2356d9e19274a0
Maria Encalada <40680775+AniiEncalada@users.noreply.github.com>
2022-01-17 16:01:55 -0500

    Add files via upload

---
+++ arbolDecision.pl
@@ -0,0 +1,14 @@
+es_cebra:-
+    es_angulado,
+    tiene_rayas_negras.
+es_angulado:- 
+    rumia, 
+    es_mamifero.
+es_angulado:- 
+    es_mamifero,
+    tiene_pezunias.
+es_mamifero.
+tiene_pezunias.
+tiene_rayas_negras.
+rumia:-fail.
+
